dc2358fe47494c58f81586038b4de350b2e12577
DanielDulcadef <dulcalito3@gmail.com>
2022-11-03 16:25:21 +0100

    Ejercicio

---
+++ ejercicio_de_cierre/main.js
@@ -0,0 +1,19 @@
+// primera1.onclick = function(){
+//     primera.hidden = true
+// }
+
+
+// segunda2.onclick = function(){
+//     segunda.hidden = true
+// }
+
+
+// tercera3.onclick = function(){
+//     tercera.hidden = true
+// }
+
+let cerrar = document.querySelectorAll(primera1)
+for (let i = 0; i < .length; i++) {
+    
+    
+}
